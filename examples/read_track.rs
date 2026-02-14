c19c2ff6152f70b04729319568f3b152d6a57fce
Seva Zaikov <seva.zaikov@gmail.com>
2026-02-13 21:33:37 -0800

    make example less brittle with the track number

--- examples/read_track.rs
+++ examples/read_track.rs
@@ -1,43 +1,39 @@
 use cd_da_reader::CdReader;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    read_cd()?;
-    Ok(())
+    let drive_path = default_drive_path();
+    read_cd(drive_path)
 }
 
 #[cfg(target_os = "windows")]
-fn read_cd() -> Result<(), Box<dyn std::error::Error>> {
-    let reader = CdReader::open(r"\\.\E:")?;
-    let toc = reader.read_toc()?;
-    println!("{:#?}", toc);
-
-    let data = reader.read_track(&toc, 11)?;
-    let wav_track = CdReader::create_wav(data);
-    std::fs::write("myfile.wav", wav_track)?;
-
-    Ok(())
+fn default_drive_path() -> &'static str {
+    r"\\.\E:"
 }
 
 #[cfg(target_os = "macos")]
-fn read_cd() -> Result<(), Box<dyn std::error::Error>> {
-    let reader = CdReader::open("disk6")?;
-    let toc = reader.read_toc()?;
-    println!("{:#?}", toc);
-
-    let data = reader.read_track(&toc, 11)?;
-    let wav_track = CdReader::create_wav(data);
-    std::fs::write("myfile.wav", wav_track)?;
-
-    Ok(())
+fn default_drive_path() -> &'static str {
+    "disk14"
 }
 
 #[cfg(target_os = "linux")]
-fn read_cd() -> Result<(), Box<dyn std::error::Error>> {
-    let reader = CdReader::open("/dev/sr0")?;
+fn default_drive_path() -> &'static str {
+    "/dev/sr0"
+}
+
+fn read_cd(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let reader = CdReader::open(path)?;
     let toc = reader.read_toc()?;
     println!("{:#?}", toc);
 
-    let data = reader.read_track(&toc, 11)?;
+    let last_audio_track = toc
+        .tracks
+        .iter()
+        .rev()
+        .find(|track| track.is_audio)
+        .ok_or_else(|| std::io::Error::other("no audio tracks in TOC"))?;
+
+    println!("Reading track {}", last_audio_track.number);
+    let data = reader.read_track(&toc, last_audio_track.number)?;
     let wav_track = CdReader::create_wav(data);
     std::fs::write("myfile.wav", wav_track)?;
 
