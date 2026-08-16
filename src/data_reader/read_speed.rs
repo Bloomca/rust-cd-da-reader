b6367f41b2df00434b3831529346b68375430d04
strict-flower <126295255+strict-flower@users.noreply.github.com>
2026-08-16 15:36:30 +0900

    Fix rustdoc

--- src/data_reader/read_speed.rs
+++ src/data_reader/read_speed.rs
@@ -1,23 +1,28 @@
-/// Representation of read speed that requested by SET CD SPEED (0xBB) command
+/// Representation of read speed requested by the SET CD SPEED (0xBB) command.
 #[derive(Debug, Clone, Copy)]
 pub enum ReadSpeed {
     /// Don't change the speed
-    /// The speed depends to the OS, previous configuration, and others
+    ///
+    /// The speed depends to the OS, previous configuration, and other factors.
     Unchanged,
 
-    /// Request to use the optimal speed
-    /// By MMC-3 specification, It can be set to the optimal speed of the drive
-    /// when it executes SET CD SPEED command with read speed (KB/s) = 0xFFFF.
-    /// On the Linux, the read speed will selected by the CDROM_SELECT_SPEED
-    /// ioctl with speed = 0. It'll set the speed automatically and highest
-    /// speed that supported by the drive.
+    /// Request the optimal speed.
+    ///
+    /// According to the MMC-3 specification, the drive can select its optimal
+    /// speed  when the set SET CD SPEED command is executed with the read
+    /// speed (KB/s) set to 0xFFFF.
+    ///
+    /// On the Linux, the read speed is selected by the CDROM_SELECT_SPEED
+    /// ioctl with speed = 0. It selects the speed automatically up to the
+    /// highest speed supported by the drive.
     Optimal,
 
-    /// Use the custom speed with specified multiplier.
+    /// Use a custom speed with the specified multiplier.
     ///
-    /// Although CD-DA reading speed should be requested by KB/s unit by the
-    /// specification, this constant will use multiplier for simple.
-    /// For example, `ReadSpeed::CustomMultiplier(10)` is a representation
-    /// of 10x speed.
+    /// Although the CD-DA read speed should be requested in KB/s according to
+    /// the specification, this variant uses a multiplier for simplicity. 
+    /// Internally, it will be calculated as `176.4 KB/s * multiplier`.
+    ///
+    /// For example, `ReadSpeed::CustomMultiplier(10)` represents 10x speed. 
     CustomMultiplier(u8),
 }
